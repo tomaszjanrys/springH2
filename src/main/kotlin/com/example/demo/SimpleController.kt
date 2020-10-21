988778fddd99a63d14d602d83b8042855d0247c4
nice <tomaszjanrys@gmail.com>
2020-10-21 17:46:06 +0200

    cd...

---
+++ src/main/kotlin/com/example/demo/SimpleController.kt
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SimpleController {
+@RequestMapping("")
+    fun getInfo():String{
+        return "info"
+    }
+}
